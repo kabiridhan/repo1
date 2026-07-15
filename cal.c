1f3df6477e623a9a643972ef8b43218481f6dd30
kabilesh <kabiridhan@gmail.com>
2026-07-15 14:31:03 +0530

    This calculator includes only addiion and subraction

---
+++ cal.c
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	int b,op;
+	printf("Enter the number 1:");
+	scanf("%d",&a);
+	printf("Enter the number 2:");
+	scanf("%d",&b);
+	printf("Menu: \n");
+	printf("1.Addition");
+	printf("2.Subraction");
+	scanf("%d",&op);
+	if(op==1)
+	{
+		printf(a+b);
+	}
+	else if(op==2)
+	{
+		printf(a-b);
+	}
+	return 0;
+}
+
